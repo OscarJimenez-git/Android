5dc8dbc8a09cee954f7852cb364705c7035c516e
2dam <oscar.jimenezmartinez@educa.madrid.org>
2025-10-02 10:18:03 +0200

    2/10

--- app/src/main/java/com/example/myapplication/domain/modelo/Usuario.kt
+++ app/src/main/java/com/example/myapplication/domain/modelo/Usuario.kt
@@ -2,4 +2,4 @@ package com.example.myapplication.domain.modelo
 
 
 
-data class Usuario(val nombre: String="", val apellido: String="",)
+data class Usuario(val nombre: String="", val apellido: String="", val Telefono: String="")
--- app/src/main/java/com/example/myapplication/ui/pantallamain/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/pantallamain/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.ui.pantallamain
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,6 +12,10 @@ import com.example.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels{
+        MainViewModelFactory()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +28,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        eventos()
+    }
+    private fun eventos(){
+        binding.guardarButton.setOnClickListener {  }
     }
 }
